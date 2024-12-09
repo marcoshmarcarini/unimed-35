620b2c616db0e57f7dbcdea67fe5f83292e729ce
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-12-09 09:10:09 -0300

    Update
    Alteração das fontes para fontes web.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
-import { UnimedSans } from "@/components/Fonts/page";
+import type { Metadata } from "next"
+import { UnimedSans } from "@/components/Fonts/page"
+import "./globals.css"
 
 
 export const metadata: Metadata = {
   title: "Unimed Sul Capuxaba",
   description: "Hotsite criado para os colaboradores expressarem todo o seu carinho para a Unimed Sul Capuxaba.",
   authors: [{ name: "Marcos Henrique Marcarini Junior" }],
-};
+}
 
 export default function RootLayout({
   children,
@@ -23,5 +22,5 @@ export default function RootLayout({
       {children}
       </body>
     </html>
-  );
+  )
 }
--- src/components/Fonts/page.tsx
+++ src/components/Fonts/page.tsx
@@ -3,22 +3,22 @@ import localFont from "next/font/local"
 export const UnimedSans = localFont({
     src: [
         {
-            path: '../../app/fonts/UnimedSans/Regular.otf',
+            path: '../../app/fonts/UnimedSans/Regular.woff2',
             weight: '400',
             style: 'normal',
         },
         {
-            path: '../../app/fonts/UnimedSans/RegularItalic.otf',
+            path: '../../app/fonts/UnimedSans/RegularItalic.woff2',
             weight: '400',
             style: 'italic',
         },
         {
-            path: '../../app/fonts/UnimedSans/Black.otf',
+            path: '../../app/fonts/UnimedSans/Black.woff2',
             weight: '900',
             style: 'normal',
         },
         {
-            path: '../../app/fonts/UnimedSans/BlackItalic.otf',
+            path: '../../app/fonts/UnimedSans/BlackItalic.woff2',
             weight: '900',
             style: 'italic',
         }
